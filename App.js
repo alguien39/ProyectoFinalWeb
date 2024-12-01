9f7b33cee9e4e5f63389917a9d9812d49ce442a7
Joham_Barberena <alguien_bama@hotmail.com>
2024-12-01 14:40:35 -0600

    Final

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const pdf = require("pdfkit");
 const fs = require("fs");
 const cors = require("cors");
 const { check, validationResult } = require("express-validator");
-
+const mysql2 = require("mysql2");
 const app = express();
 
 // Middleware
@@ -15,3 +15,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const db = require('./src/Connection.js');
+const { Console } = require('console');
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        return;
+    }
+    console.log('Conexión exitosa a la base de datos');
+});
+
+// Obtener todas las peliculas
+app.get('/Peliculas', (req, res) => {
+    const query = 'SELECT * FROM Peliculas';
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error al obtener datos:', err);
+            res.status(500).send('Error al obtener datos');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
